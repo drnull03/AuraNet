080876a0e89008a15c556b8c5edb8484b547aa32
Diaa Hanna <diaa.251202@gmail.com>
2026-06-17 16:08:41 +0000

    Added missing namespace in pulumi layer 2

--- pulumi_IoC/2-auranet/__main__.py
+++ pulumi_IoC/2-auranet/__main__.py
@@ -3,19 +3,28 @@ import pulumi_kubernetes as k8s
 
 print(f"🐝 Pulumi is configuring AuraNet Layer 2 (NATS Messaging Engine)...")
 
-# 1. Pull the Kubeconfig from Layer 1 (StackReference)
-# Format: "<organization_name>/<project_name>/<stack_name>"
-# If using the default local backend, you can typically use just "dev" or your local stack name path.
+#  Pull the Kubeconfig from Layer 1 (StackReference)
+
 infra_reference = pulumi.StackReference("dev") 
 kubeconfig_from_layer1 = infra_reference.get_output("raw_kubeconfig")
 
-# 2. Instantiate a K8s Provider using Layer 1's dynamic cluster connection
+# Instantiate a K8s Provider using Layer 1's dynamic cluster connection
 k8s_provider = k8s.Provider(
     "layer2-k8s-provider",
     kubeconfig=kubeconfig_from_layer1
 )
 
-# 3. Create a dedicated namespace for NATS to isolate system messaging
+
+# Create the Main Application Namespace 
+app_namespace = k8s.core.v1.Namespace(
+    "auranet-app-namespace",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="auranet-namespace"
+    ),
+    opts=pulumi.ResourceOptions(provider=k8s_provider)
+)
+
+# Create a dedicated namespace for NATS to isolate system messaging
 nats_namespace = k8s.core.v1.Namespace(
     "nats-namespace",
     metadata=k8s.meta.v1.ObjectMetaArgs(
@@ -24,7 +33,7 @@ nats_namespace = k8s.core.v1.Namespace(
     opts=pulumi.ResourceOptions(provider=k8s_provider)
 )
 
-# 4. Deploy highly available Central NATS Cluster via Helm
+# Deploy highly available Central NATS Cluster via Helm
 nats_release = k8s.helm.v3.Release(
     "auranet-nats-broker",
     k8s.helm.v3.ReleaseArgs(
@@ -48,6 +57,7 @@ nats_release = k8s.helm.v3.Release(
 )
 
 # Export layer outputs for layer 3 or validation checking
+pulumi.export("app_namespace", app_namespace.metadata.name)
 pulumi.export("nats_namespace", nats_namespace.metadata.name)
 pulumi.export("nats_status", nats_release.status.name)
 pulumi.export("nats_connection_string", "nats://my-nats.auranet-messaging.svc.cluster.local:4222")
